2d2e4f5fd7883ea685582ec0e7874974472bdfdd
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-01 22:14:38 -0500

    I am finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
@@ -6,14 +8,24 @@ public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
+       Main obj = new Main();
 
         // call the compute() method on that new object
+        obj.compute();
     }
 
     public void compute() {
         // Get input string of name from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
         
         // Create conditional to check name against Alice and Bob
+        if(name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")){
+            System.out.println("Hello, " + name + "! How are yay?");
+        }else{
+            System.out.println("You are neither Alice nor Bob");
+        }
 
         // Print greeting to screen if Alice or Bob are true
         
